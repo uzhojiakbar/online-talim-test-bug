a25494e7fbaeeb1f837c9e3a8d2a1a6e23534da4
uzhojiakbar <uzhojiakbar3@gmail.com>
2025-03-16 01:35:30 +0500

    API chanmged

--- src/Hooks/api.jsx
+++ src/Hooks/api.jsx
@@ -1,19 +1,19 @@
-import axios from 'axios'
-import { getCookie } from './getCooce'
+import axios from "axios";
+import { getCookie } from "./getCooce";
 export const instance = axios.create({
-    baseUrl: "http://localhost:3000"
-})
+  baseUrl: "http://37.27.215.130:5013",
+});
 
 instance.interceptors.request.use(
-    (config) => {
-        const token = getCookie("token");
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`;
-        }
-        return config;
-    },
-    (error) => {
-        console.log('Request xatosi:', error);
-        return Promise.reject(error);
+  (config) => {
+    const token = getCookie("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
     }
+    return config;
+  },
+  (error) => {
+    console.log("Request xatosi:", error);
+    return Promise.reject(error);
+  }
 );
--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:3000', // Proksi sifatida ishlatiladigan server manzili
+      "/api": {
+        target: "http://37.27.215.130:5013", // Proksi sifatida ishlatiladigan server manzili
         changeOrigin: true, // Serverga kelayotgan so'rovni o'zgartirish
         secure: false, // Agar HTTPS bo'lsa, buni true qilib qo'yishingiz mumkin
         // pathRewrite: { '^/api': '' }, // Agar yo'lni o'zgartirmoqchi bo'lsangiz
       },
     },
   },
-  
-})
+});
