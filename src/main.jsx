d85f6039a92ab1019519c0e1b51889e4798128ae
uzhojiakbar <uzhojiakbar3@gmail.com>
2025-03-17 01:05:07 +0500

    1

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import Root from './root/Root'
-import './index.css'
-import { Toaster } from 'react-hot-toast'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { createRoot } from "react-dom/client";
+import Root from "./root/Root";
+import "./index.css";
+import { Toaster } from "react-hot-toast";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { StrictMode } from "react";
 
 const queryClient = new QueryClient();
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-      <QueryClientProvider client={queryClient}>
-        <Root />
-        <Toaster />
-      </QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <Root />
+      <Toaster />
+    </QueryClientProvider>
   </StrictMode>
-)
+);
